fac99c379c1f1b9899bbf01c929e5e3328b378b7
fzwbgj478q-coder <fzwbgj478q@privaterelay.appleid.com>
2026-09-17 09:18:12 +0800

    fix: restore Qt MOC generation in CI

--- src/window/StudyWindow.h
+++ src/window/StudyWindow.h
@@ -2,5 +2,27 @@
 #include <QDialog>
 #include "service/StorageService.h"
 class QLabel; class QListWidget; class QLineEdit; class QDateEdit; class QCheckBox;
-class StudyWindow final: public QDialog { Q_OBJECT public: explicit StudyWindow(QWidget*parent=nullptr); void setData(const UserData&, const QDate&today); void setTimerText(const QString&); signals: void startRequested();void pauseRequested();void resumeRequested();void resetRequested();void todoSaved(const TodoItem&);void todoToggled(const QString&,bool);void todoRemoved(const QString&); private: void loadSelectedTodo(); QLabel *m_timer,*m_today,*m_week,*m_stats; QListWidget*m_todos;QLineEdit*m_title;QDateEdit*m_due;QCheckBox*m_hasDue; };
+class StudyWindow final : public QDialog
+{
+    Q_OBJECT
+public:
+    explicit StudyWindow(QWidget *parent = nullptr);
+    void setData(const UserData &data, const QDate &today);
+    void setTimerText(const QString &text);
+signals:
+    void startRequested();
+    void pauseRequested();
+    void resumeRequested();
+    void resetRequested();
+    void todoSaved(const TodoItem &item);
+    void todoToggled(const QString &id, bool completed);
+    void todoRemoved(const QString &id);
+private:
+    void loadSelectedTodo();
+    QLabel *m_timer = nullptr, *m_today = nullptr, *m_week = nullptr, *m_stats = nullptr;
+    QListWidget *m_todos = nullptr;
+    QLineEdit *m_title = nullptr;
+    QDateEdit *m_due = nullptr;
+    QCheckBox *m_hasDue = nullptr;
+};
 
